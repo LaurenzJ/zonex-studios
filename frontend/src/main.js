e1cb9fd2e205a9fec07caccb7f7cc86407f07e37
Laurent Brand <laurentbrand19@gmail.com>
2022-08-10 18:13:40 +0200

    removed vue-prlx

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,8 @@ import App from './App.vue'
 import './assets/tailwind.css'
 import router from './router'
 
-import VuePrlx from 'vue-prlx'
-App.use(VuePrlx);
+// import { VuePrlx } from 'vue-prlx'
+// App.use(VuePrlx);
 
 import 'flowbite';
 
